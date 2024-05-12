8f8b2d6b414fc1083fcdb5e5433eb0f34b2f9ae4
Er Anees Akbar <eraneesakbar@gmail.com>
2024-05-12 11:07:55 +0530

    student request fixed

--- admin/students.php
+++ admin/students.php
@@ -7,53 +7,53 @@
 
 <body>
     <?php
-         include 'menu.php';
-         include 'header.php';
-         
-         ?>
+    include 'menu.php';
+    include 'header.php';
+
+    ?>
     <div style="background-color: whitesmoke; width:1030px;height: 800px;margin-left: 230px;">
         <div style=" padding:90px;">
             <h1 style="text-align: center; font-family: 'Lobster', cursive;color: #333"> Student Regristration Requests
             </h1>
             <br />
             <?php
-               include('../db.php');
-               $res = mysqli_query($con, "select * from users where status = 0 ");
-               if (mysqli_num_rows($res) > 0) 
-               {
-                 echo "<table class='table table-striped table-bordered'>";
-                 echo "<tr class='success'>";
-                 echo "<th>" . "Name" . "</th>";
-                 echo "<th>" . "Email Id" . "</th>";
-                 echo "<th>" . "Mobile" . "</th>";
-                 echo "<th>" . "Experience" . "</th>";
-                 echo "<th>" . "Address" . "</th>";
-                 echo "<th>" . "About" . "</th>";
-                 echo "<th>" . "Action" . "</th>";
-                 echo "</tr>";
+            include('../db.php');
+            $res = mysqli_query($con, "select * from users where status = 0 ");
+            echo "<table class='table table-striped table-bordered'>";
+            if (mysqli_num_rows($res) > 0) {
+                echo "<tr class='success'>";
+                echo "<th>" . "Name" . "</th>";
+                echo "<th>" . "Email Id" . "</th>";
+                echo "<th>" . "Mobile" . "</th>";
+                echo "<th>" . "Experience" . "</th>";
+                echo "<th>" . "Address" . "</th>";
+                echo "<th>" . "About" . "</th>";
+                echo "<th>" . "Action" . "</th>";
+                echo "</tr>";
 
-                  while ($r = mysqli_fetch_assoc($res))
-                  {
-                   echo "<tr>";
-                   echo "<td>" . $r['name'] . "</td>";
-                   echo "<td>" . $r['email'] . "</td>";
-                   echo "<td>" . $r['mobile'] . "</td>";
-                   echo "<td>" . $r['experience'] . "</td>";
-                   echo "<td>" . $r['address'] . "</td>";
-                   echo "<td>" . $r['about'] . "</td>";
-                   echo   "<td>" . "<a class='btn btn-success' href='confirm-student.php?id=" . $r['id'] . "'> Confirm </a>" . "</td>";
-                   echo "</tr>";
-                  }
-                 echo "</table>";
+                while ($r = mysqli_fetch_assoc($res)) {
+                    echo "<tr>";
+                    echo "<td>" . $r['name'] . "</td>";
+                    echo "<td>" . $r['email'] . "</td>";
+                    echo "<td>" . $r['mobile'] . "</td>";
+                    echo "<td>" . $r['experience'] . "</td>";
+                    echo "<td>" . $r['address'] . "</td>";
+                    echo "<td>" . $r['about'] . "</td>";
+                    echo   "<td>" . "<a class='btn btn-success' href='confirm-student.php?id=" . $r['id'] . "'> Confirm </a>" . "</td>";
+                    echo "</tr>";
                 }
-              ?>
+            } else {
+                echo "<tr><td class='text-center'><i>No request pending...</i></td></tr>";
+                echo "</table>";
+            }
+            ?>
         </div>
     </div>
 </body>
 
 </html>
 <style>
-body {
-    background-color: whitesmoke;
-}
+    body {
+        background-color: whitesmoke;
+    }
 </style>
